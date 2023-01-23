238838a0d6a44074389fe14c2e664f20463b71fe
Narayan <narayanbala@gmail.com>
2023-01-23 17:16:00 +0530

    buffer keymap updates to nvim

--- .config/nvim/lua/narayan/core/keymaps.lua
+++ .config/nvim/lua/narayan/core/keymaps.lua
@@ -29,8 +29,10 @@ keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+-- keymap("n", "<S-l>", ":bnext<CR>", opts)
+-- keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<S-l>", "<CMD>BufferLineCycleNext<CR>", opts)
+keymap("n", "<S-h>", "<CMD>BufferLineCyclePrev<CR>", opts)
 
 -- Close buffers
 keymap("n", "<S-q>", "<cmd>Bdelete!<CR>", opts)
