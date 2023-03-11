833f5ffe19130f122d8762652bf676bbc0f17a16
Narayan <narayanbala@gmail.com>
2023-03-11 15:27:29 +0530

    removed shell scripts folder

--- shell-scripts/bin/cproj.sh
+++
@@ -1,4 +0,0 @@
-clang-format --style=google --dump-config > .clang-format
-sed -i "s/\(ColumnLimit:\s\+\)80/\1100/" .clang-format
-sed -i "s/\(ColumnLimit:\s\+\)80/\1100/" .clang-format
-sed -i "s/\(SeparateDefinitionBlocks:\s\+\)Leave/\1Always/" .clang-format
--- shell-scripts/bin/pyproj.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp ../py/flake8 .flake8
-cp ../py/gitignore .gitignore
-poetry init -n 
-cat ../py/pyproject.toml >> pyproject.toml
