033f0c50ee6a91c30a370c1a444bc32e90323eba
Narayan <narayanbala@gmail.com>
2024-11-16 12:43:58 +0530

    Updated welcome screen

--- .config/customnvim/lua/plugins/alpha.lua
+++ .config/customnvim/lua/plugins/alpha.lua
@@ -1,6 +1,9 @@
 return {
 	"goolord/alpha-nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+	dependencies = { "echasnovski/mini.icons" },
+	event = "VimEnter",
+	enabled = true,
+	init = false,
 	config = function()
 		local alpha = require("alpha")
 		local dashboard = require("alpha.themes.dashboard")
@@ -23,6 +26,33 @@ return {
 			dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua<CR>"),
 			dashboard.button("q", "  Quit NVIM", ":qa<CR>"),
 		}
+		-- close Lazy and re-open when the dashboard is ready
+		if vim.o.filetype == "lazy" then
+			vim.cmd.close()
+			vim.api.nvim_create_autocmd("User", {
+				once = true,
+				pattern = "AlphaReady",
+				callback = function()
+					require("lazy").show()
+				end,
+			})
+		end
 		alpha.setup(dashboard.opts)
+		vim.api.nvim_create_autocmd("User", {
+			once = true,
+			pattern = "LazyVimStarted",
+			callback = function()
+				local stats = require("lazy").stats()
+				local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+				dashboard.section.footer.val = "⚡ Neovim loaded "
+					.. stats.loaded
+					.. "/"
+					.. stats.count
+					.. " plugins in "
+					.. ms
+					.. "ms"
+				pcall(vim.cmd.AlphaRedraw)
+			end,
+		})
 	end,
 }
