5ea4fba34e33cde4967e63e00859b1a28d8fa1ce
Narayan <narayanbala@gmail.com>
2024-12-15 08:51:16 +0530

    Updated configs

--- .config/customnvim/lua/plugins/colorscheme.lua
+++ .config/customnvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,30 @@
 return {
-	{"vague2k/vague.nvim", },
-	{"sainnhe/edge",},
-	{"sainnhe/gruvbox-material",},
-	{ "EdenEast/nightfox.nvim", opts = {} },
-	{ "ilof2/posterpole.nvim", config = function() vim.cmd([[ colorscheme vague ]]) end, },
+	{ "AlexvZyl/nordic.nvim" },
+	{ "narayanbs/mylake.nvim" },
+	{ "rrethy/base16-nvim" },
+	{ "daschw/leaf.nvim", opts = { contrast = "low" } },
+	{
+		"ellisonleao/gruvbox.nvim", -- theme
+		opts = {
+			bold = true,
+			palette_overrides = {
+				bright_green = "#b8bb26",
+				bright_red = "#fb4934",
+				bright_orange = "#d65d0e",
+				bright_yellow = "#fabd2f",
+			},
+			overrides = {
+				Function = { fg = "#83a598" },
+			},
+		},
+	},
+	{
+		"sainnhe/sonokai",
+		config = function()
+			if vim.opt.termguicolors:get() then
+				vim.opt.termguicolors = false
+			end
+			vim.cmd("colorscheme sonokai")
+		end,
+	},
 }
--- .config/nvim/lua/plugins/mycustom.lua
+++ .config/nvim/lua/plugins/mycustom.lua
@@ -1,108 +1,133 @@
 return {
-	-- colorschemes
-	{ "rmehri01/onenord.nvim" },
-	{ "AlexvZyl/nordic.nvim" },
-	{ "vague2k/vague.nvim" },
-	{ "ilof2/posterpole.nvim" },
-	{ "f4z3r/gruvbox-material.nvim" },
-	-- Configure LazyVim to load colorscheme
-	{
-		"LazyVim/LazyVim",
-		opts = { colorscheme = "vague" },
-	},
-	-- neo-tree Configuration
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		opts = {
-			window = {
-				width = 25,
-			},
-		},
-	},
+  -- colorschemes
+  -- {"RRethy/base16-nvim"},
+  { "AlexvZyl/nordic.nvim" },
+  { "narayanbs/mylake.nvim" },
+  { "rrethy/base16-nvim" },
+  { "daschw/leaf.nvim", opts = { contrast = "low" } },
+  {
+    "ellisonleao/gruvbox.nvim", -- theme
+    opts = {
+      bold = true,
+      palette_overrides = {
+        bright_green = "#b8bb26",
+        bright_red = "#fb4934",
+        bright_orange = "#d65d0e",
+        bright_yellow = "#fabd2f",
+      },
+      overrides = {
+        Function = { fg = "#83a598" },
+      },
+    },
+  },
+  {
+    "sainnhe/sonokai",
+    config = function()
+      if vim.opt.termguicolors:get() then
+        vim.opt.termguicolors = false
+      end
+      vim.cmd("colorscheme sonokai")
+    end,
+  },
 
-	-- treesitter configuration
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = {
-			ensure_installed = {
-				"bash",
-				"html",
-				"javascript",
-				"json",
-				"lua",
-				"markdown",
-				"markdown_inline",
-				"python",
-				"tsx",
-				"typescript",
-				"go",
-				"cpp",
-				"rust",
-			},
-		},
-	},
+  -- Load the colorscheme
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = { colorscheme = "sonokai" },
+  -- },
+  -- neo-tree Configuration
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = {
+        width = 25,
+      },
+    },
+  },
 
-	-- lsp configuration
-	{
-		"neovim/nvim-lspconfig",
-		---@class PluginLspOpts
-		opts = {
-			inlay_hints = { enabled = false },
-			document_highlight = {
-				enabled = false,
-			},
-			servers = {
-				clangd = {
-					mason = false,
-				},
-				-- These servers will be automatically installed by mason and loaded with lspconfig
-				pyright = {},
-				gopls = {},
-				ts_ls = {},
-				tailwindcss = {},
-				emmet_ls = {},
-			},
-		},
-	},
+  -- treesitter configuration
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "tsx",
+        "typescript",
+        "go",
+        "cpp",
+        "rust",
+      },
+    },
+  },
 
-	-- formatters configuration
-	{
-		"stevearc/conform.nvim",
-		opts = {
-			formatters_by_ft = {
-				lua = { "stylua" },
-				python = { "isort", "black" },
-				go = { "goimports", "gofmt" },
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescriptreact = { "prettier" },
-				css = { "prettier" },
-				html = { "prettier" },
-				json = { "prettier" },
-				yaml = { "prettier" },
-				markdown = { "prettier" },
-				graphql = { "prettier" },
-			},
-		},
-	},
-	-- colorizer for css/tailwind
-	{
-		"NvChad/nvim-colorizer.lua",
-		opts = {
-			user_default_options = {
-				tailwind = true,
-			},
-		},
-	},
-	-- Zenmode for focussed writing/coding
-	{
-		"folke/zen-mode.nvim",
-		keys = function()
-			return {
-				{ "<leader>zm", "<cmd>ZenMode<cr>", desc = "Zen Mode Toggle" },
-			}
-		end,
-		opts = {},
-	},
+  -- lsp configuration
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      inlay_hints = { enabled = false },
+      document_highlight = {
+        enabled = false,
+      },
+      servers = {
+        clangd = {
+          mason = false,
+        },
+        -- These servers will be automatically installed by mason and loaded with lspconfig
+        pyright = {},
+        gopls = {},
+        ts_ls = {},
+        tailwindcss = {},
+        emmet_ls = {},
+      },
+    },
+  },
+
+  -- formatters configuration
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "isort", "black" },
+        go = { "goimports", "gofmt" },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+        graphql = { "prettier" },
+      },
+    },
+  },
+  -- colorizer for css/tailwind
+  {
+    "NvChad/nvim-colorizer.lua",
+    opts = {
+      user_default_options = {
+        tailwind = true,
+      },
+    },
+  },
+  -- Zenmode for focussed writing/coding
+  {
+    "folke/zen-mode.nvim",
+    keys = function()
+      return {
+        { "<leader>zm", "<cmd>ZenMode<cr>", desc = "Zen Mode Toggle" },
+      }
+    end,
+    opts = {},
+  },
 }
