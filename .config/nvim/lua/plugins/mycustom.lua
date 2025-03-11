1a6f2ebda73cee738debcd5589aeb7c4a319d627
Narayan <narayanbala@gmail.com>
2025-03-11 15:36:31 +0530

    Updated snacks nvim config

--- .config/nvim/lua/plugins/mycustom.lua
+++ .config/nvim/lua/plugins/mycustom.lua
@@ -1,123 +1,127 @@
 return {
-  -- colorschemes
-  --{ "rrethy/base16-nvim" },
-  {
-    "ellisonleao/gruvbox.nvim", -- theme
-    opts = {
-      bold = false,
-      -- palette_overrides = {
-      --   bright_green = "#b8bb26",
-      --   bright_red = "#fb4934",
-      --   bright_orange = "#d65d0e",
-      --   bright_yellow = "#fabd2f",
-      -- },
-      -- overrides = {
-      --   Function = { fg = "#83a598" },
-      -- },
-    },
-  },
-  { "navarasu/onedark.nvim", opts = { style = "warmer" } },
-  { "whizikxd/naysayer-colors.nvim" },
-  -- Load the colorscheme
-  {
-    "LazyVim/LazyVim",
-    opts = { colorscheme = "catppuccin-frappe" },
-  },
-  -- neo-tree Configuration
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      window = {
-        width = 25,
-      },
-    },
-  },
+	-- colorschemes
+	--{ "rrethy/base16-nvim" },
+	{
+		"ellisonleao/gruvbox.nvim", -- theme
+		opts = {
+			bold = false,
+			-- palette_overrides = {
+			--   bright_green = "#b8bb26",
+			--   bright_red = "#fb4934",
+			--   bright_orange = "#d65d0e",
+			--   bright_yellow = "#fabd2f",
+			-- },
+			-- overrides = {
+			--   Function = { fg = "#83a598" },
+			-- },
+		},
+	},
+	{ "navarasu/onedark.nvim", opts = { style = "warmer" } },
+	{ "whizikxd/naysayer-colors.nvim" },
+	{ "olivercederborg/poimandres.nvim", opts = {} },
 
-  -- treesitter configuration
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "bash",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "tsx",
-        "typescript",
-        "go",
-        "cpp",
-        "rust",
-        "zig",
-      },
-    },
-  },
+	-- Load the colorscheme
+	{
+		"LazyVim/LazyVim",
+		opts = { colorscheme = "naysayer" },
+	},
+	-- neo-tree Configuration
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		opts = {
+			window = {
+				width = 25,
+			},
+		},
+	},
 
-  -- lsp configuration
-  {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      inlay_hints = { enabled = false },
-      document_highlight = {
-        enabled = false,
-      },
-      servers = {
-        clangd = {
-          mason = false,
-        },
-        -- These servers will be automatically installed by mason and loaded with lspconfig
-        pyright = {},
-        gopls = {},
-        ts_ls = {},
-        tailwindcss = {},
-        emmet_ls = {},
-        zls = {},
-      },
-    },
-  },
+	-- treesitter configuration
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"bash",
+				"html",
+				"javascript",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"tsx",
+				"typescript",
+				"go",
+				"cpp",
+				"rust",
+				"zig",
+			},
+		},
+	},
 
-  -- formatters configuration
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        lua = { "stylua" },
-        python = { "isort", "black" },
-        go = { "goimports", "gofmt" },
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
-        css = { "prettier" },
-        html = { "prettier" },
-        json = { "prettier" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
-        graphql = { "prettier" },
-      },
-    },
-  },
-  -- colorizer for css/tailwind
-  {
-    "NvChad/nvim-colorizer.lua",
-    opts = {
-      user_default_options = {
-        tailwind = true,
-      },
-    },
-  },
-  -- Zenmode for focussed writing/coding
-  {
-    "folke/zen-mode.nvim",
-    keys = function()
-      return {
-        { "<leader>zm", "<cmd>ZenMode<cr>", desc = "Zen Mode Toggle" },
-      }
-    end,
-    opts = {},
-  },
+	-- lsp configuration
+	{
+		"neovim/nvim-lspconfig",
+		---@class PluginLspOpts
+		opts = {
+			inlay_hints = { enabled = false },
+			document_highlight = {
+				enabled = false,
+			},
+			servers = {
+				clangd = {
+					mason = false,
+				},
+				-- These servers will be automatically installed by mason and loaded with lspconfig
+				pyright = {},
+				gopls = {},
+				ts_ls = {},
+				tailwindcss = {},
+				emmet_ls = {},
+				zls = {},
+			},
+		},
+	},
+
+	-- formatters configuration
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "isort", "black" },
+				go = { "goimports", "gofmt" },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				graphql = { "prettier" },
+			},
+		},
+	},
+	-- colorizer for css/tailwind
+	{
+		"NvChad/nvim-colorizer.lua",
+		opts = {
+			user_default_options = {
+				tailwind = true,
+			},
+		},
+	},
+	-- Zenmode for focussed writing/coding
+	{
+		"folke/zen-mode.nvim",
+		keys = function()
+			return {
+				{ "<leader>zm", "<cmd>ZenMode<cr>", desc = "Zen Mode Toggle" },
+			}
+		end,
+		opts = {},
+	},
+	-- enable to prevent highlighting of words under cursor
+	--{ "snacks.nvim", opts = { words = { enabled = false } } },
 }
