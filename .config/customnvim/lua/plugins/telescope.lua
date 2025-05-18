770222d9a73d2a61ac39211bef89b8fc2a0f0f72
Narayan <narayanbala@gmail.com>
2025-05-18 07:31:46 +0530

    Updated telescope lua in customnvim

--- .config/customnvim/lua/plugins/telescope.lua
+++ .config/customnvim/lua/plugins/telescope.lua
@@ -7,6 +7,7 @@ return {
 		config = function()
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>fr", builtin.oldfiles, {})
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 		end,
 	},
