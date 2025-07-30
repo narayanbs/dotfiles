bd45c29b7fa6ea561d36183a85a8ed1b13ef13ac
Narayan <narayanbala@gmail.com>
2025-07-30 07:40:26 +0530

    Updated mason config

--- .config/latestnvim/lua/plugins/mason.lua
+++ .config/latestnvim/lua/plugins/mason.lua
@@ -3,4 +3,13 @@ return {
 		"mason-org/mason.nvim",
 		opts = {},
 	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+    opts = {
+        ensure_installed = { "lua_ls", "gopls", "pyright", "emmet_language_server", "ts_ls" },
+    },
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {} },
+	},
+},
 }
