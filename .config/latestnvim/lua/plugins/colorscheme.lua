c5daf8eae21dab808bc68d999530892236f8e809
Narayan <narayanbala@gmail.com>
2025-08-15 19:31:51 +0530

    Updated alacritty and neovim config

--- .config/latestnvim/lua/plugins/colorscheme.lua
+++ .config/latestnvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,6 @@
 return {
-	{ "narayanbs/mylake.nvim"  },
-	{ 'RostislavArts/naysayer.nvim'},
+	{ "narayanbs/mylake.nvim" },
+	{ "RostislavArts/naysayer.nvim" },
+	{ "cpea2506/one_monokai.nvim" },
+	{ "daschw/leaf.nvim", opts = { contrast = "medium" } },
 }
